f1aafdf1ab08838f3552f0939995e2a78bdddcca
Tim Losito <losito105@gmail.com>
2021-02-27 21:29:33 -0500

    setup connection to LCD

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,16 +1,22 @@
-import pyfirmata
 import time
 import speech_recognition as sr
 import pokemon
 
-board = pyfirmata.Arduino("/dev/cu.usbmodem14201")
+# RASPBERRY PI SETUP
+import board
+import digitalio
+lcd_rs = digitalio.DigitalInOut(board.D26)
+lcd_en = digitalio.DigitalInOut(board.D19)
+lcd_d7 = digitalio.DigitalInOut(board.D27)
+lcd_d6 = digitalio.DigitalInOut(board.D22)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d4 = digitalio.DigitalInOut(board.D25)
 
-# BLINK PROGRAM
-# while True:
-#     board.digital[13].write(1)
-#     time.sleep(1)
-#     board.digital[13].write(0)
-#     time.sleep(1)
+lcd_columns = 16
+lcd_rows = 2
+
+import adafruit_character_lcd.character_lcd as characterlcd
+lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
 
 # SPEECH TO TEXT
 r = sr.Recognizer()
@@ -20,6 +26,7 @@ with sr.Microphone() as source:
     try:
         text = r.recognize_google(audio)
         entry = pokemon.pokedex[text]
-        # TODO: print entry.name and entry.desc to LCD module
+        print(entry.name + "\n" + entry.desc)
+        lcd.message = entry.name + "\n" + entry.desc
     except:
         print("voice not recognized")
